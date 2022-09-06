57eb46aaa9c37012d153b0110e6f9bd5461706fe
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-09-06 18:34:10 -0500

    pagina de editar

--- src/components/EditarProducto.jsx
+++ src/components/EditarProducto.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const EditarProducto = () => {
-  return <div>EditarProducto</div>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">Editar producto</h2>
+
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Nombre:</label>
+                <input
+                  className="form-control"
+                  name="name"
+                  id="name"
+                  type="text"
+                  placeholder="Nombre del producto"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="price">Precio:</label>
+                <input
+                  className="form-control"
+                  name="price"
+                  id="price"
+                  type="number"
+                  placeholder="Precio del producto"
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Editar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditarProducto;
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Productos = () => {
   return (
-    <div>
-      Productos
-    </div>
+    <>
+      <h2 className="text-center my-5">Listado de Productos</h2>
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
   );
-}
+};
 
 export default Productos;
