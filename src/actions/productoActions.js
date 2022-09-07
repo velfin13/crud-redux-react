8073a177645e682ddecbde2cd97f1603986c7cf0
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-09-06 21:45:39 -0500

    flujo de trabajo deredux

---
+++ src/actions/productoActions.js
@@ -0,0 +1,30 @@
+import {
+  AGREGAR_PRODUCTO,
+  AGREGAR_PRODUCTO_EXITO,
+  AGREGAR_PRODUCTO_ERROR,
+} from "../types";
+
+// crear nuevos productos
+export const crearNuevoProductoAction = (producto) => {
+  return (dispatch) => {
+    dispatch(agregarProducto());
+
+    try {
+      dispatch(agregarProductoExito(producto));
+    } catch (error) {
+      dispatch(agregarProductoError(true));
+    }
+  };
+};
+
+const agregarProducto = () => ({ type: AGREGAR_PRODUCTO, payload: true });
+
+const agregarProductoExito = (producto) => ({
+  type: AGREGAR_PRODUCTO_EXITO,
+  payload: producto,
+});
+
+const agregarProductoError = (error) => ({
+  type: AGREGAR_PRODUCTO_ERROR,
+  payload: error,
+});
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+//acciones de redux
+import { crearNuevoProductoAction } from "../actions/productoActions";
 
 const NuevoProducto = () => {
+  // state del componente
+  const [name, setname] = useState("");
+  const [price, setprice] = useState(0);
+
+  //utilizar dispatch y te crea una funcion
+  const dispatch = useDispatch();
+
+  //mandar a llamar el action de productoActions
+  const agregarProducto = (producto) =>
+    dispatch(crearNuevoProductoAction(producto));
+
+  const submitNuevoProducto = (e) => {
+    e.preventDefault();
+
+    //validar formulario
+    if (name.trim() === "" || price <= 0) {
+      return;
+    }
+
+    //si no hay errores
+
+    //crear producto
+    agregarProducto({ name, price });
+  };
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -8,13 +36,15 @@ const NuevoProducto = () => {
           <div className="card-body">
             <h2 className="text-center mb-4 font-weight-bold">Agregar nuevo</h2>
 
-            <form>
+            <form onSubmit={submitNuevoProducto}>
               <div className="form-group">
                 <label htmlFor="name">Nombre:</label>
                 <input
                   className="form-control"
                   name="name"
                   id="name"
+                  value={name}
+                  onChange={(e) => setname(e.target.value)}
                   type="text"
                   placeholder="Nombre del producto"
                 />
@@ -25,6 +55,8 @@ const NuevoProducto = () => {
                   className="form-control"
                   name="price"
                   id="price"
+                  value={price}
+                  onChange={(e) => setprice(Number(e.target.value))}
                   type="number"
                   placeholder="Precio del producto"
                 />
